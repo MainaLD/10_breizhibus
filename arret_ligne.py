5adc1796186d686f388efd586cdc9bb755f14606
MainaLD <maina.ledem@gmail.com>
2022-03-03 09:58:41 +0100

    ajout class ligne, bus et arrêts

---
+++ arret_ligne.py
@@ -0,0 +1,7 @@
+
+class Arret_ligne : 
+    def __init__(self, nom_ligne, nom_arrets, adresse):
+        self.nom_ligne = nom_ligne
+        self.nom_arrets = nom_arrets
+        self.adresse = adresse
+    
---
+++ bus.py
@@ -0,0 +1,12 @@
+
+class Bus : 
+    def __init__(self, id_bus, numero, immatriculation, nombre_place, id_ligne, nom_ligne, ):
+        self.id_bus = id_bus
+        self.numero = numero                
+        self.immat = immatriculation       
+        self.nb_place = nombre_place
+        self.id_ligne = id_ligne
+        self.nom_ligne = nom_ligne    
+    
+    
+    # id_bus, numero, immatriculation, nombre_place, bus.id_ligne, lignes.nom
